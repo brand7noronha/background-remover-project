cbd865db7fcaad631d82e5e32bd9830d5b736f25
brand7noronha <brandonnoronha7+github@gmail.com>
2026-06-06 11:21:14 +0530

    Update CORS for Github Pages

--- backend/api/main.py
+++ backend/api/main.py
@@ -12,8 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "https://brand7noronha.github.io/background-remover-project/",
     ],
     allow_methods=["POST"],
     allow_headers=["*"],
